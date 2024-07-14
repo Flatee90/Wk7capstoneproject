2f810836df87c7f241b6c4127a5b71129f964ddd
Folashade Lateef <flatee90@gmail.com>
2024-07-13 19:47:25 -0500

    update

--- mommy/mommy.js
+++ mommy/mommy.js
@@ -1,10 +1,10 @@
-function mommyretreat(){
+function submitOrder() {
     let name;
     let guest;
     name = prompt("What is your name?");
     guest = prompt("A mommy you want to treat?");
     newyearsHeader.innerHTML = `You're Invited....`;
     newyearsParagraph.innerHTML = `Hi ${guest}! ${name} would like to invite you to treat you to a day of pampering!  `;
-    
-  }
-    mommyretreat.onclick = mommyretreat
+
+}
+mommyretreat.onclick = submitOrder
