7f066a82aa8d8bd915dbb4b0cdd6982c329d3942
Folashade Lateef <flatee90@gmail.com>
2024-07-17 14:24:46 -0500

    update

--- mommy/mommy.js
+++ mommy/mommy.js
@@ -7,9 +7,17 @@ function myappointmentscheduler() {
     appointment = prompt("What day would you like to book?");
     style = prompt("hair,nails, massage, full mommy makeover?");
     mommyHeader.innerHTML = `You're Invited....`;
-    myappointmentHeader.innerHTML = `someone booked a spa day you!`
+    myappointmentHeader.innerHTML = `someone booked a spa day for you!`
     myappointmentParagraph.innerHTML = `Hi ${name}, someone wants to treat you to a spa day for ${appointment} for ${style} we can't wait to see you!`;
 
 }
 mommyspa.onclick = myappointmentscheduler
+
+function treattotal() {
+    let treattotal = 50;
+    let nails = 15;
+    let hair = 15;
+    let massage = 20;
+    if (treatsubtotal === 50) text = `your total is $50`;
+} 
 
